2ae3f74313bd9d01daf89f489f7cec8d9d657b0c
Thomas Edwards <thce28@gmail.com>
2019-02-14 12:46:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -79,10 +79,12 @@ def get_average_age_for_season(data, season)
       girl_data.each do |info|
         
       ages += info["age"].to_f
-      binding.pry 
+       
       end
     end
   end
+  num_con
   ages
+  binding.pry 
   
 end
