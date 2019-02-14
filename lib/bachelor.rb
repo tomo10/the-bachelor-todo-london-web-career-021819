5272a07d9fd5930910c9872b3a71a83a6ac94a9e
Thomas Edwards <thce28@gmail.com>
2019-02-14 12:34:39 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,91 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |year, girl_data|
+    if year == season 
+      girl_data.each do |info|
+      
+        info.each do |profile, x|
+        
+          if x == "Winner"
+            
+          return info["name"].split.first
+         
+          end 
+        end
+      end
+    end
+  end    
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |year, girl_data|
+    
+    girl_data.each do |info|
+      
+      info.each do |profile, x|
+        
+        if x == occupation
+          
+        return info["name"]
+        
+        
+        end
+      end    
+    end
+  end  
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0 # number of contestants from that hometown 
+  data.each do |year, girl_data|
+    
+    girl_data.each do |info|
+      
+      info.each do |profile, x|
+     
+        if x == hometown
+        counter += 1
+        
+        end
+      end
+    end    
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |year, girl_data|
+    
+    girl_data.each do |info|
+      
+      info.each do |profile, x|
+     
+        if x == hometown
+        return info["occupation"]
+        
+        end
+      end
+    end    
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = 0 
+  
+  data.each do |year, girl_data|
+    if year == season
+      
+      girl_data.each do |info|
+        
+      ages += info["age"].to_f
+        
+        
+          
+      binding.pry   
+      
+        
+      end
+    end
+  end
+  ages / girl_data.size 
 end
